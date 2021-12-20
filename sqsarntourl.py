2a90911d72f3732c19faec76f75359a0df191c54
Zapfmeister <zapfmeister@gmail.com>
2021-12-20 10:19:17 +0100

    Add error handling

--- sqsarntourl.py
+++ sqsarntourl.py
@@ -1,11 +1,17 @@
-eventSourceARN = input("Please provide the SQS ARN:")
-StrSplit = eventSourceARN.split(":")
-service = StrSplit[2]
-region = StrSplit[3]
-accountId = StrSplit[4]
-queueName = StrSplit[5]
-queueUrl = "https://" + service + "." + region + ".amazonaws.com/" + accountId + '/' + queueName
-print("\n")
-print("======================================================================================")
-print("SQS URL: " + queueUrl)
-print("======================================================================================")
+import sys
+
+try:
+    eventSourceARN = input("Please provide the SQS ARN:")
+    StrSplit = eventSourceARN.split(":")
+    service = StrSplit[2]
+    region = StrSplit[3]
+    accountId = StrSplit[4]
+    queueName = StrSplit[5]
+    queueUrl = "https://" + service + "." + region + ".amazonaws.com/" + accountId + '/' + queueName
+    print("\n")
+    print("======================================================================================")
+    print("SQS URL: " + queueUrl)
+    print("======================================================================================")
+except:
+    print("Invalid input")
+    print("Syntax is supposed to be: arn:aws:sqs:us-east-2:123456789012:MyTopic")
